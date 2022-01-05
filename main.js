f8af11b26f72caa1431d52c2983312fb6a6a33ca
Valentin <Beaury.valentin@gmail.com>
2022-01-05 05:18:58 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,62 @@
+// main.js
+
+// Modules to control application life and create native browser window
+const { app,window, BrowserWindow } = require('electron')
+const path = require('path')
+
+const createWindow = () => {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  // et charger l'index.html de l'application.
+  mainWindow.loadFile('index.html')
+
+  // Ouvrir les outils de développement.
+  // mainWindow.webContents.openDevTools()
+}
+
+// Cette méthode sera appelée quand Electron aura fini
+// de s'initialiser et sera prêt à créer des fenêtres de navigation.
+// Certaines APIs peuvent être utilisées uniquement quant cet événement est émit.
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+// Quitter quand toutes les fenêtres sont fermées, sauf sur macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+
+// preload.js
+
+// Toutes les APIs Node.js sont disponibles dans le processus de préchargement.
+// Il a la même sandbox qu'une extension Chrome.
+window.addEventListener('DOMContentLoaded', () => {
+    const replaceText = (selector, text) => {
+      const element = document.getElementById(selector)
+      if (element) element.innerText = text
+    }
+  
+    for (const dependency of ['chrome', 'node', 'electron']) {
+      replaceText(`${dependency}-version`, process.versions[dependency])
+    }
+  })
+
+  
+// In this file you can include the rest of your app's specific main process
+// code. Vous pouvez également le mettre dans des fichiers séparés et les inclure ici.
